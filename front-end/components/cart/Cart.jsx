5a2d095898b360ea0bfe00d2f21f10bd40501088
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-10-14 08:20:01 +0800

    little fix but not

--- front-end/components/cart/Cart.jsx
+++ front-end/components/cart/Cart.jsx
@@ -1,20 +1,28 @@
-"use client"
-import { createContext,useContext } from "react/cjs/react.development"
-const CartContext=createContext(null)
-export const CartProvider=({children})=>{
-const stringifiedCartProducts=localStorage.getItem('cartProducts');
-const cartProducts=stringifiedCartProducts? JSON.parse(stringifiedCartProducts):[];
-return (
-    <CartContext.Provider value={cartProducts}>
-        {children}
-    </CartContext.Provider>
-)
-}
-export const useCart=()=>{
-    const context=useContext(CartContext);
-    if(cartProducts===undefined)
-    {
-        throw new Error('useCart must be used within a Cartprovider')
-    }
-    return context
-}
+"use client";
+import {
+  createContext,
+  useContext,
+  useReducer,
+  useMemo,
+} from "react/cjs/react.development";
+const CartContext = createContext(null);
+export const CartProvider = ({ children }) => {
+  const stringifiedCartProducts = localStorage.getItem("cartProducts");
+  const cartProducts = stringifiedCartProducts
+    ? JSON.parse(stringifiedCartProducts)
+    : [];
+  
+  const value = useMemo(() => ({
+    cart: cartProducts,
+  }));
+  return (
+    <CartContext.Provider value={cartProducts}>{children}</CartContext.Provider>
+  );
+};
+export const useCart = () => {
+  const context = useContext(CartContext);
+  if (cartProducts === undefined) {
+    throw new Error("useCart must be used within a Cartprovider");
+  }
+  return [context];
+};
