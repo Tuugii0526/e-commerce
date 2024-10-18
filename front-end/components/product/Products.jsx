46db011fae9e1a4b87d6f57c46d1845ef8fd31d4
Tuugii <namjildorjtuguldur1234@gmail.com>
2024-10-19 01:05:36 +0800

    As the first configuration on typescript , i wrote my code on javascript. It resulted in conflict .

--- front-end/components/product/Products.jsx
+++
@@ -1,9 +0,0 @@
-import { ProductsSub } from "./ProductsSub";
-
-export const Products = async () => {
-  const res = await fetch(`${process.env.BACKEND_URL}/products`);
-    const products = await res.json();
-  return (
-    <ProductsSub products={products}/>
-  );
-};
